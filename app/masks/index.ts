a93428a131c0ee70a2614ab8ecbcd18305aef822
Modestas <modestaso@gmail.com>
2024-08-19 13:45:48 +0300

    only en masks

--- app/masks/index.ts
+++ app/masks/index.ts
@@ -34,7 +34,7 @@ if (typeof window != "undefined") {
     })
     .then((masks) => {
       const { cn = [], tw = [], en = [] } = masks;
-      return [...cn, ...tw, ...en].map((m) => {
+      return [...en].map((m) => {
         BUILTIN_MASKS.push(BUILTIN_MASK_STORE.add(m));
       });
     });
